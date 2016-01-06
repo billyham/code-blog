7d8c7a08efc51f67458ea08543318749ebd8fa60
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2016-01-05 20:58:35 -0800

    Basic functionality is complete

---
+++ scripts/project.js
@@ -0,0 +1,51 @@
+// An arry of project objects
+var projects = [];
+
+function Project(dataObject){
+  this.title = dataObject.title;
+  this.body = dataObject.body;
+  this.dateOfCreation = dataObject.dateOfCreation;
+};
+
+Project.prototype.toHtml = function(){
+
+  // Make a copy of the template projet text
+  var $newProject = $('article#template').clone();
+
+  // Console log for caveman debugging
+  // console.log('I\'m alive');
+
+  // Remove the id attribute
+  $newProject.removeAttr('id');
+
+  // Add the title
+  $newProject.find('.projectTitle').text(this.title);
+
+  // Add the relative Date
+  $newProject.find('.projectDate').text(this.dateOfCreation);
+
+  // Add the body text
+  $newProject.find('.projectBody').text(this.body);
+
+  return $newProject;
+};
+
+// Sort the array of projectData based on the dateOfCreation
+projectData.sort(function(date1,date2){
+  return (new Date(date2.dateOfCreation) - new Date(date1.dateOfCreation));
+});
+
+// Iterate through array of raw data and push a new project object in the array of projects
+for (i=0; i<projectData.length; i++){
+  projects.push(new Project(projectData[i]));
+}
+
+// Iterate through the projects array and create HTML text using the object data
+for (var i in projects){
+  // Using jQuery to identify the HTML section of projects
+  // And adding a block of HTML for each project object
+  $('#projectsSection').append(projects[i].toHtml());
+};
+
+// Finally, delete the template project
+$('article#template').remove();
---
+++ scripts/projectData.js
@@ -0,0 +1,13 @@
+var projectData= [
+  {
+    title: 'First Project',
+    body: 'I made some omelets today. They were yummy',
+    dateOfCreation: '2015-12-02'
+  },
+  {
+    title: 'Second Project',
+    body: 'My cat loves to chase her mouse toy.',
+    dateOfCreation: '2016-1-05'
+  }
+
+];
