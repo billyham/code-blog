289794489e220004a9d10a1b804a52d49cee2989
Dave Hanagan <billyxshenaniganfilms@hotmail.com>
2016-01-09 01:36:12 -0800

    Added the show more button and functionality

--- scripts/project.js
+++ scripts/project.js
@@ -5,10 +5,10 @@ function Project(dataObject){
   this.category = dataObject.category;
   this.body = dataObject.body;
   this.dateOfCreation = dataObject.dateOfCreation;
+  this.additionalText = dataObject.additionalText;
 };
 
 Project.prototype.toHtml = function(){
-
   // Make a copy of the template projet text
   var $newProject = $('article#template').clone();
 
@@ -37,5 +37,42 @@ Project.prototype.toHtml = function(){
   // Add the body text
   $newProject.find('.projectBody').text(this.body);
 
+  // Determine if additional text exists
+  if (this.additionalText){
+    // Save the additional text as data
+    $newProject.attr('data-additionalText', this.additionalText);
+
+    // Add the event handler when show more is tapped
+    $newProject.find('a').on('click', showMoreHandler);
+  }else{
+    // No additional text, so hide the show more anchor
+    $newProject.find('a').remove();
+  }
+  
   return $newProject;
+};
+
+// Handler function for when a project's "show more" is tapped
+function showMoreHandler(e){
+  // Prevent the default action of the anchor tag
+  e.preventDefault();
+
+  // Cache the parent element
+  $parentElement = $(e.target).parent();
+
+  // Determine if it should show more or less
+  if ($(e.target).text() === 'show more'){
+    // The additional text
+    var additionalText = $parentElement.attr('data-additionalText');
+    // Add additional text to the preceding element
+    $(e.target).prev().after('<p>' + additionalText) + '</p>';
+    // Change the <a> tag to show less
+    $(e.target).text('show less');
+  }else{
+    // Remove the additional text
+    $(e.target).prev().remove();
+    // Change the <a> tag back to show more
+    $(e.target).text('show more');
+  }
+
 };
--- scripts/projectData.js
+++ scripts/projectData.js
@@ -3,25 +3,28 @@ var projectData= [
     title: 'Film to Digital Transfer Station',
     category: 'Film',
     body: 'Using a modified HVX200 camera (described below), I created a system for the digital capture of images from Super 8 and 16mm film. Software made for a Mac is the controller, receiving a video stream from the camera and operating a film projector (formerly part of an optical printer). It uses an Arduino microcontroller to manage the communication between the Mac application and the film projector.',
-    dateOfCreation: '2015-12-02'
+    dateOfCreation: '2015-12-02',
+    additionalText: 'I hereby assume full responsibility for the above listed equipment provided by the Northwest Film Center. Financial responsibility includes payment for all repairs, up to the full replacement value of equipment, and the full replacement value for all stolen or lost equipment. Financial responsibility also includes the rental fee for the time period in which damaged equipment is out for repair, or until replacement payment is received. I have inspected the contents of rental equipment and acknowledge that all parts and pieces are present and in working order unless otherwise noted.'
   },
   {
     title: 'Transistor Radio with guts replaced by iPod and Arduino',
     category: 'Music',
     body: 'I had always felt a desire to interact with the music player on my iPad in the same way I interact with a transistor radio. By making a custom iPad/iPhone app and using an Arduino microcontroller, I can control the music using a tuner dial. At start up, the iPad generates 11 "stations" of music using the music library on the device, and I surf the channels using the very same tuner dial that had come from a transistor radio. I even went so far as to package the entire apparatus inside the shell of a tabletop radio to make it fully convincing.',
-    dateOfCreation: '2016-1-05'
+    dateOfCreation: '2016-1-05',
+    additionalText: 'I hereby assume full responsibility for the above listed equipment provided by the Northwest Film Center. Financial responsibility includes payment for all repairs, up to the full replacement value of equipment, and the full replacement value for all stolen or lost equipment. Financial responsibility also includes the rental fee for the time period in which damaged equipment is out for repair, or until replacement payment is received. I have inspected the contents of rental equipment and acknowledge that all parts and pieces are present and in working order unless otherwise noted.'
   },
   {
     title: 'iPad App for Rental Equipment Checkout',
     category: 'Productivity',
     body: 'A custom iPad app for managing equipment inventory and checkout procedures. The iPad uses a shared server as a data source so that many iPads can be in use among multiple staff members at any one time. Equipment is labeled with QR codes for easy scanning to look up or checkout pieces of gear. ',
-    dateOfCreation: '2016-1-07'
+    dateOfCreation: '2016-1-07',
+    additionalText: 'I hereby assume full responsibility for the above listed equipment provided by the Northwest Film Center. Financial responsibility includes payment for all repairs, up to the full replacement value of equipment, and the full replacement value for all stolen or lost equipment. Financial responsibility also includes the rental fee for the time period in which damaged equipment is out for repair, or until replacement payment is received. I have inspected the contents of rental equipment and acknowledge that all parts and pieces are present and in working order unless otherwise noted.'
   },
   {
     title: 'Reverb: Video Installation',
     category: 'Film',
     body: 'A video installation I co-created with artist Donnabelle Casis as part of New Works Lab, an initiative by Seattle\'s 911 Media Arts Center. Light from opposing video projectors intermingle as they cast images of a woman sleeping and bees swarming onto the semi-transparent surface of screens made from sequins. A rhythmic sound, reminiscent of cicada, rises an falls in sync with the swarming of bees. Light dances around the entire space, which we designed to be inviting and comforting. Audiences were at ease to sit or lie down on a foam covered floor and allow the images and sounds to creep into their unconscious.',
-    dateOfCreation: '2015-11-08'
+    dateOfCreation: '2015-11-08',
+    additionalText: ''
   }
-
 ];
